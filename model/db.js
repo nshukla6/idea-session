9150775a944c6d8ac1221ed7a9bc733bb266ac48
nshukla6 <nshukla6@gmail.com>
2016-05-13 14:08:47 +0530

    Update db.js

--- model/db.js
+++ model/db.js
@@ -1,5 +1,7 @@
 var chalk = require('chalk');
 var mongoose = require( 'mongoose' );
+var bcrypt=require('bcrypt');
+var SALT_WORK_FACTOR = 10;
 
 
 
@@ -43,6 +45,36 @@ var storiesSchema = new mongoose.Schema({
   comments:[{body:String,commented_by:String,date:Date}],
   slug:String
 });
+
+userSchema.pre('save', function(next) {
+    var user = this;
+    console.log("Before Registering the user");
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        console.log("Salt");
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            console.log("Hash : "+hash);
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 // Build the User model
 
